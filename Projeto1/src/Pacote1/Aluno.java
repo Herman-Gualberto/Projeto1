da3eab3afa594a77b38cdcce557084fc0bd8e9f7
LVMU2C631 <LVMU2C631@IBM-PE07R44E>
2022-09-02 20:34:13 -0300

    Projeto1 criado

--- Projeto1/src/Pacote1/Aluno.java
+++ Projeto1/src/Pacote1/Aluno.java
@@ -3,6 +3,7 @@ package Pacote1;
 public class Aluno {
 	public static void main(String[] args) {
 		System.out.println("Primeira mensagem no Java");
+		System.out.println("Agora estou no GitHub");
 	}
 
 }
